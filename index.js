a0934c301b0390e5980e0127e2fd176300148892
Ish J <ish@knights.ucf.edu>
2021-09-16 21:05:42 -0700

    Continuing work on the chrome extension

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-function saveClicked(){
-    console.log("Hey dude we got clicked")
+let myLeads = ["www.ask1.com", "www.ask2.com", "www.ask3.com"]
+const inputEL = document.getElementById("inputEl")
+const inputBtnn = document.getElementById("inputBtn")
+
+inputBtnn.addEventListener("click", function(){
+    myLeads.push(inputEL.value)
+    console.log(myLeads)
+})
+
+for(let x = 0; x < myLeads.length; x++) {
+    console.log(myLeads[x])
 }
